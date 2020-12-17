b2ea04e482435b95eed61a9b4c63a53035802bb8
Jim O'Connor <joconnor@fastmail.us>
2020-12-16 22:37:16 -0700

    Now keeping the two string tables distinct
    These are .strtab and .shstrtab.  At present it is not protected
    against a third string table.  .shstrtab itself is distinguished by
    the index value in the top elf header.

--- elfwalk.cpp
+++ elfwalk.cpp
@@ -15,44 +15,28 @@ std::map<int, std::string> stt_name;
 std::map<int, std::string> stb_name;
 std::map<int, std::string> sht_name;
 std::map<int, std::string> strtab;
+std::map<int, std::string> shstrtab;
 std::map<int, std::string> symtab;
 
 static void init()
 {
-  sht_name[SHT_NULL] = "NULL";
-  sht_name[SHT_SYMTAB] = "SYMTAB";
-  sht_name[SHT_STRTAB] = "STRTAB";
-  sht_name[SHT_RELA] = "RELA";
-  sht_name[SHT_PROGBITS] = "PROGBITS";
-  sht_name[SHT_NOBITS] = "NOBITS";
-  sht_name[SHT_NOTE] = "NOTE";
+  sht_name[SHT_NULL]      = "NULL";
+  sht_name[SHT_SYMTAB]    = "SYMTAB";
+  sht_name[SHT_STRTAB]    = "STRTAB";
+  sht_name[SHT_RELA]      = "RELA";
+  sht_name[SHT_PROGBITS]  = "PROGBITS";
+  sht_name[SHT_NOBITS]    = "NOBITS";
+  sht_name[SHT_NOTE]      = "NOTE";
 
-  stt_name[STT_NOTYPE] = "NOTYPE";
-  stt_name[STT_OBJECT] = "OBJECT";
-  stt_name[STT_FUNC] = "FUNC";
-  stt_name[STT_SECTION] = "SECTION";
-  stt_name[STT_FILE] = "FILE";
+  stt_name[STT_NOTYPE]    = "NOTYPE";
+  stt_name[STT_OBJECT]    = "OBJECT";
+  stt_name[STT_FUNC]      = "FUNC";
+  stt_name[STT_SECTION]   = "SECTION";
+  stt_name[STT_FILE]      = "FILE";
 
-  stb_name[STB_LOCAL] = "LOCAL";
-  stb_name[STB_GLOBAL] = "GLOBAL";
-  stb_name[STB_WEAK] = "WEAK";
-
-}
-
-static bool rdelf(FILE* file, void* ptr, size_t len)
-{
-  size_t size = fread(ptr, len, 1, file);
-  if (size != 1) {
-    if (feof(file)) {
-      printf("EOF detected on file\n");
-    } else if (ferror(file)) {
-      printf("error on fread(%ld)\n", size);
-    } else {
-      printf("unknown condition on fread\n");
-    }
-    return false;
-  }
-  return true;
+  stb_name[STB_LOCAL]     = "LOCAL";
+  stb_name[STB_GLOBAL]    = "GLOBAL";
+  stb_name[STB_WEAK]      = "WEAK";
 }
 
 static bool check_ident(unsigned char ident[EI_NIDENT])
@@ -82,24 +66,18 @@ bool parse_symtab(char* buffer, long size, long entsize)
   return true;
 }
 
-void parse_strtab(char* buffer, long size)
+void parse_strtab(char* buffer, long size, std::map<int, std::string>& table)
 {
   printf("[%s]\n", __func__);
   buffer++;
   size--;
   int npos = 1;
   while (size > 0) {
-    auto n = printf("%s ", buffer);
-    strtab[npos] = buffer;
-    int k = strtab[npos].size() + 1;
-    int m = strlen(buffer) + 1;
+    table[npos] = buffer;
+    int n = table[npos].size() + 1;
     npos += n;
-    printf("%d %d %d\n", n,m,k);
     size -= n;
     buffer += n;
-  }
-  for (auto p = strtab.begin(); p != strtab.end(); p++) {
-    printf("%d %s\n", p->first, p->second.data());
   }
 }
 
@@ -124,14 +102,17 @@ bool process_file(void* _vPtr)
   }
 
   Elf64_Shdr* shdr = (Elf64_Shdr*)(buffer + ehdr->e_shoff);
-  for (int i=0; i<ehdr->e_shnum; i++) {
+  for (int secno=0; secno<ehdr->e_shnum; secno++) {
     switch (shdr->sh_type) {
     case SHT_SYMTAB: {
       parse_symtab(buffer+shdr->sh_offset, shdr->sh_size, shdr->sh_entsize);
       break;
     }
     case SHT_STRTAB: {
-      parse_strtab(buffer+shdr->sh_offset, shdr->sh_size);
+      if (secno == ehdr->e_shstrndx)
+	parse_strtab(buffer+shdr->sh_offset, shdr->sh_size, shstrtab);
+      else
+	parse_strtab(buffer+shdr->sh_offset, shdr->sh_size, strtab);
       break;
     }
     default:
@@ -140,78 +121,6 @@ bool process_file(void* _vPtr)
     shdr++;
   }
   return true;
-}
-
-int old_process_file(const char* filename)
-{
-  FILE* file = fopen(filename, "r");
-  if (file == nullptr) {
-    printf("Unable to open file\n");
-    return -1;
-  }
-
-  Elf64_Ehdr ehdr;
-  if (!rdelf(file, &ehdr, sizeof(ehdr)))
-    return -1;
-
-  if (!check_ident(ehdr.e_ident))
-      return -1;
-  if (ehdr.e_ident[EI_CLASS] != ELFCLASS64) {
-    printf("only support ELF 64 for now\n");
-    return -1;
-  }
-  if (ehdr.e_type != ET_REL) {
-    printf("Not relocatable object\n");
-    return -1;
-  }
-  if (ehdr.e_machine != EM_X86_64) {
-    printf("Not AMD x86-64 arch\n");
-    return -1;
-  }
-
-  if (ehdr.e_shoff) {
-    if (fseek(file, ehdr.e_shoff, SEEK_SET) != 0) {
-      printf("Unable to seek to offset e_shoff=%ld\n", ehdr.e_shoff);
-      return -1;
-    }
-    Elf64_Shdr shdr[ehdr.e_shnum];
-    if (sizeof(Elf64_Shdr) != ehdr.e_shentsize) {
-      printf("Error section header entry size incorrect\n");
-      return -1;
-    }
-    if (fread(&shdr, ehdr.e_shentsize, ehdr.e_shnum, file) != ehdr.e_shnum) {
-      printf("Failed to read all %d section headers\n", ehdr.e_shnum);
-      return -1;
-    }
-    Elf64_Shdr *symtab, *strtab;
-    for (int i=0; i<ehdr.e_shnum; i++) {
-      switch (shdr[i].sh_type) {
-      case SHT_SYMTAB:
-	{
-	  symtab = &shdr[i];
-	  printf("i=%d type=%d\n", i, symtab->sh_type);
-	  break;
-	}
-      case SHT_STRTAB:
-	{
-	  strtab = &shdr[i];
-	  printf("i=%d type=%d\n", i, strtab->sh_type);
-	  fseek(file, strtab->sh_offset, SEEK_SET);
-	  char buffer[strtab->sh_size+1];
-	  fread(buffer, strtab->sh_size, 1, file);
-	  break;
-	}
-      default:
-	break;
-      }
-      // auto st_type = ELF64_ST_TYPE(shdr.sh_info);
-      // auto st_bind = ELF64_ST_BIND(shdr.sh_info);
-      // printf("type=%d bind=%d\n", st_type, st_bind);
-      // printf("type=%s bind=%s\n", stt_name[st_type].data(), stb_name[st_bind].data());
-    }
-  }
-
-  return 0;
 }
 
 int main(int argc, char** argv)
@@ -245,7 +154,14 @@ int main(int argc, char** argv)
     printf("failed\n");
     exit(1);
   }
+  printf(">>> strtab\n");
+  for (auto p = strtab.begin(); p != strtab.end(); p++) {
+    printf("%d %s\n", p->first, p->second.data());
+  }
+  printf(">>> shstrtab\n");
+  for (auto p = shstrtab.begin(); p != shstrtab.end(); p++) {
+    printf("%d %s\n", p->first, p->second.data());
+  }
 
-  printf("success\n");
   exit(0);
 }
