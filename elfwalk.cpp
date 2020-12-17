d4e2868d444c680b58bb02f46a4d619ce1bcdb36
Jim O'Connor <joconnor@fastmail.us>
2020-12-16 22:05:58 -0700

    string parsing is almost there

--- elfwalk.cpp
+++ elfwalk.cpp
@@ -4,21 +4,30 @@
 #include "elf.h"
 #include <map>
 #include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <cstring>
 
 std::map<int, std::string> stt_name;
 std::map<int, std::string> stb_name;
-std::map<int, std::string> sh_name;
+std::map<int, std::string> sht_name;
+std::map<int, std::string> strtab;
+std::map<int, std::string> symtab;
 
 static void init()
 {
-  sh_name[SHT_NULL] = "NULL";
-  sh_name[SHT_SYMTAB] = "SYMTAB";
-  sh_name[SHT_STRTAB] = "STRTAB";
-  sh_name[SHT_RELA] = "RELA";
-  sh_name[SHT_PROGBITS] = "PROGBITS";
-  sh_name[SHT_NOBITS] = "NOBITS";
-  sh_name[SHT_NOTE] = "NOTE";
+  sht_name[SHT_NULL] = "NULL";
+  sht_name[SHT_SYMTAB] = "SYMTAB";
+  sht_name[SHT_STRTAB] = "STRTAB";
+  sht_name[SHT_RELA] = "RELA";
+  sht_name[SHT_PROGBITS] = "PROGBITS";
+  sht_name[SHT_NOBITS] = "NOBITS";
+  sht_name[SHT_NOTE] = "NOTE";
 
+  stt_name[STT_NOTYPE] = "NOTYPE";
   stt_name[STT_OBJECT] = "OBJECT";
   stt_name[STT_FUNC] = "FUNC";
   stt_name[STT_SECTION] = "SECTION";
@@ -54,7 +63,86 @@ static bool check_ident(unsigned char ident[EI_NIDENT])
 	  ident[EI_MAG3] == ELFMAG3);
 }
 
-static int process_file(const char* filename)
+bool parse_symtab(char* buffer, long size, long entsize)
+{
+  printf("[%s]\n", __func__);
+  if (entsize != sizeof(Elf64_Sym)) {
+    printf("symtab entry invalid size\n");
+    return false;
+  }
+
+  auto symhdr = (Elf64_Sym*)buffer;
+  int nelem = size/entsize;
+  for (int i=0; i<nelem; i++,symhdr++) {
+    printf("%d %s %s \n", i,
+	   stb_name[ELF64_ST_BIND(symhdr->st_info)].data(),
+	   stt_name[ELF64_ST_TYPE(symhdr->st_info)].data());
+    // st_name indexes into the symbol table
+  }
+  return true;
+}
+
+void parse_strtab(char* buffer, long size)
+{
+  printf("[%s]\n", __func__);
+  buffer++;
+  size--;
+  int npos = 1;
+  while (size > 0) {
+    auto n = printf("%s ", buffer);
+    strtab[npos] = buffer;
+    int k = strtab[npos].size() + 1;
+    int m = strlen(buffer) + 1;
+    npos += n;
+    printf("%d %d %d\n", n,m,k);
+    size -= n;
+    buffer += n;
+  }
+  for (auto p = strtab.begin(); p != strtab.end(); p++) {
+    printf("%d %s\n", p->first, p->second.data());
+  }
+}
+
+bool process_file(void* _vPtr)
+{
+  printf("%s\n", __func__);
+
+  auto buffer = (char*)_vPtr;
+  auto ehdr = (Elf64_Ehdr*)buffer;
+  if (!check_ident(ehdr->e_ident)) {
+    fprintf(stderr, "invalid header\n");
+    return false;
+  }
+
+  if (ehdr->e_type != ET_REL) {
+    printf("Not relocatable object\n");
+    return false;
+  }
+  if (ehdr->e_machine != EM_X86_64) {
+    printf("Not AMD x86-64 arch\n");
+    return false;
+  }
+
+  Elf64_Shdr* shdr = (Elf64_Shdr*)(buffer + ehdr->e_shoff);
+  for (int i=0; i<ehdr->e_shnum; i++) {
+    switch (shdr->sh_type) {
+    case SHT_SYMTAB: {
+      parse_symtab(buffer+shdr->sh_offset, shdr->sh_size, shdr->sh_entsize);
+      break;
+    }
+    case SHT_STRTAB: {
+      parse_strtab(buffer+shdr->sh_offset, shdr->sh_size);
+      break;
+    }
+    default:
+      break;
+    }
+    shdr++;
+  }
+  return true;
+}
+
+int old_process_file(const char* filename)
 {
   FILE* file = fopen(filename, "r");
   if (file == nullptr) {
@@ -134,9 +222,30 @@ int main(int argc, char** argv)
   }
 
   init();
-  
-  if (process_file(argv[1]))
-    exit(1);
 
+  int fd = open(argv[1], O_RDWR);
+  if (fd < 0) {
+    perror("open");
+    exit(1);
+  }
+
+  struct stat statbuf;
+  if (fstat(fd, &statbuf)) {
+    perror("stat");
+    exit(1);
+  }
+  printf("size of %s is %ld bytes\n", argv[1], statbuf.st_size);
+  auto elfptr = mmap(NULL, statbuf.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (elfptr == MAP_FAILED) {
+    perror("mmap");
+    exit(1);
+  }
+  
+  if (!process_file(elfptr)) {
+    printf("failed\n");
+    exit(1);
+  }
+
+  printf("success\n");
   exit(0);
 }
